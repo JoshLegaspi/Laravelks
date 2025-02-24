eb209d7fb9b8acac7dcec9ac6cee921999e5c33b
John Joshua Legaspi <s2202116@usls.edu.ph>
2025-02-24 08:52:47 +0800

    Feat: Making page responsive and Integrating Bootstrap

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\InventoryController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+
+
+Route::get('/inventory', [InventoryController::class, 'index'])->name('inventory');
+
+Route::get('/add', [CreateController::class, 'index'])->name('add');
